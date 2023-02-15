c1f74e70fe5cf3658696f67f4790e237d9d1d4a1
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-02-15 17:48:38 -0300

    criando do contact controller

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,24 @@
+class ContactController {
+  index() {
+    // listar
+  }
+
+  show() {
+    // obter um registro
+  }
+
+  store() {
+    // criando novo registro
+  }
+
+  update() {
+    // editar um registro
+  }
+
+  delete() {
+    // deletar um registro
+  }
+}
+
+// singleton
+module.exports = new ContactController()
