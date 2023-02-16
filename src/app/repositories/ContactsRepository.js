3e00a638419c667229b40990984a60fe523e8a17
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-02-16 15:51:55 -0300

    criação da rota "delete"- deletando por id

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,6 +1,6 @@
 const { v4 } = require('uuid')
 
-const contacts = [
+let contacts = [
   {
     id: v4(),
     name: 'Vittor',
@@ -27,6 +27,13 @@ class ContactsRepository {
   findById(id) {
     return new Promise((resolve, reject) => {
       resolve(contacts.find((contact) => contact.id === id))
+    })
+  }
+
+  delete(id) {
+    return new Promise((resolve, reject) => {
+      contacts = contacts.filter((contact) => contact.id !== id)
+      resolve()
     })
   }
 }
