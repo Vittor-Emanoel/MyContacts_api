9cee620a7450b8cc19da7aa867e11eea5e3c7eb7
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-02-23 14:56:23 -0300

    refatoração do metodo create

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,10 +1,11 @@
 const { v4 } = require('uuid')
+const db = require('../../database')
 
 let contacts = [
   {
     id: v4(),
     name: 'Vittor',
-    email: 'vittor@gmail.com',
+    email: 'vittodddr@gmail.com',
     phone: '123456789',
     category_id: v4(),
   },
@@ -43,18 +44,12 @@ class ContactsRepository {
     })
   }
 
-  create({ name, email, phone, category_id }) {
-    return new Promise((resolve, reject) => {
-      const newContact = {
-        id: v4(),
-        name,
-        email,
-        phone,
-        category_id,
-      }
-      contacts.push(newContact)
-      resolve(newContact)
-    })
+  async create({ name, email, phone, category_id }) {
+    const [rows] = await db.query(
+      'INSERT INTO contacts(name, email, phone, category_id) VALUES ($1, $2, $3, $4) RETURNING * ',
+      [name, email, phone, category_id]
+    )
+    return rows
   }
 
   update(id, { name, email, phone, category_id }) {
