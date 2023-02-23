4e9b27945686fec437ede887a182e981093636e8
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-02-23 00:27:30 -0300

    conectando o Node ao Postgres

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE mycontacts;
 
-CREATE EXTENSION  ID NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS categories (
     id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
