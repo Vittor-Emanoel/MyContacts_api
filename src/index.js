a2c42cbb463126e2527fbbd6ee61d8e705d0de43
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-02-25 02:13:23 -0300

    Capturando e tratando erros

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ const routes = require('./routes')
 
 const app = express()
 
+// Middlewares
 app.use(express.json())
 app.use(routes)
+
+// error handler
+app.use((error, request, response, next) => {
+  console.log(error)
+  response.sendStatus(500)
+})
 
 app.listen(3333, () =>
   console.log(' 🔥 Server started at http://localhost:3333')
