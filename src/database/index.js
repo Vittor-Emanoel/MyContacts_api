85efcb6a8a826f9a0ebc63319fff44d34ae5d3fb
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-04-10 22:59:51 -0300

    teste deploy

--- src/database/index.js
+++ src/database/index.js
@@ -1,16 +1,31 @@
-const { Client } = require('pg')
+require('dotenv').config()
+console.log(process.env.DATABASE_URL)
+const {Pool} = require('@neondatabase/serverless')
 
-const client = new Client({
-  host: 'localhost',
-  port: 5432,
-  user: 'root',
-  password: 'root',
-  database: 'mycontacts',
-})
-
-client.connect()
-
-exports.query = async (query, values) => {
-  const { rows } = await client.query(query, values)
-  return rows
+module.exports = {
+  async fetch(req, env, ctx) {
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const { rows: [{ now }] } = await pool.query('SELECT now()');
+    ctx.waitUntil(pool.end());
+    return new Response(now);
+  }
 }
+
+
+
+
+// const { Client } = require('pg')
+
+// const client = new Client({
+//   host: 'postgres://Vittor-Emanoel:q4AmbPoLgn7x@ep-delicate-block-399797.us-east-2.aws.neon.tech/mycontacts',
+//   user: 'Vittor-Emanoel',
+//   password: '7BPisgzqcG4N',
+//   database: 'mycontacts',
+// })
+
+// client.connect()
+
+// exports.query = async (query, values) => {
+//   const { rows } = await client.query(query, values)
+//   return rows
+// }
