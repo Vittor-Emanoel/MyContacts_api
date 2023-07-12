e69c875dba30eb8c418e87fabbc9b31e998ba211
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-07-11 22:57:56 -0300

    refactor: validate if the uuid

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -99,6 +99,10 @@ class ContactController {
   async delete(request, response) {
     const { id } = request.params;
 
+    if(!isValidUUID(id)) {
+      return response.status(400).json({ error: 'Invalid user id' })
+    }
+
     await ContactsRepository.delete(id);
     // no content
     response.sendStatus(204);
